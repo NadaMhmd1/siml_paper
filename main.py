8d5495dcf8cdfce42437bdc93e84f8eb3b0331eb
NadaMhmd1 <137094583+NadaMhmd1@users.noreply.github.com>
2024-03-15 04:22:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 20:08:36 2024
+
+@author: nado
+"""
+
+
+from Sim_1 import run_sim
+from ML_reg import run_reg
+
+def main():
+    run_sim()
+    run_reg()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
